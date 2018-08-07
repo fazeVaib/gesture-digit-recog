e46746bc54d7957de0031eac73b4fc769f780950
fazevaib <vaibhavvats123@gmail.com>
2018-08-08 00:28:10 +0530

    commented the code

--- camera_pred.py
+++ camera_pred.py
@@ -1,81 +1,112 @@
-import cv2 as cv
-import numpy as np 
+import cv2 as cv # for image processing and input
+import numpy as np # For matrix calculations
 import math
 import time
 from skimage.feature import hog
 from sklearn.externals import joblib
-from collections import deque
+from collections import deque # for center points to be used later on
 
+# capturing video using the default camera
 cap = cv.VideoCapture(0)
+
+# centerpoints are used in order to plot the data on the new board from image
 centerpoints = deque()
+
+# assigning result some temporary negative values
 result = [-1, -2]
 
+# Defining the intensity of green color to be identified.
+# It is in HSV format
 sensitivity = 20
 lgreen = np.array([60-sensitivity, 100, 50])
 ugreen = np.array([60+sensitivity, 255, 255])
 
+# Black-Board where identified image from video is to be drawn
 board = np.zeros((300, 300), dtype='uint8')
 
+# importing classifier and preprocessed data from the earlier saved model
 clf, preproc = joblib.load('mnist_clf_svm.pkl')
 
+# Function to return the contours within the image
+def get_ict(roi):  
+    roi = cv.GaussianBlur(roi, (7, 7), 0) # it softens the image thus making it a bit blurry
+    hsv_roi = cv.cvtColor(roi, cv.COLOR_BGR2HSV) # converting to HSV color format
 
-def get_ict(roi):  # returns image, contours and threshold
-    roi = cv.GaussianBlur(roi, (7, 7), 0)
-    hsv_roi = cv.cvtColor(roi, cv.COLOR_BGR2HSV)
+    #filtering out the color 
     roi_range = cv.inRange(hsv_roi, lgreen, ugreen)
+
+    # Finding contours based on the detected green color
     _, contour, hier = cv.findContours(
         roi_range.copy(), cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
     return roi, contour, roi_range
 
+# main() starts:
 while cap.isOpened():
+	# read each and every frame
     _, frame = cap.read()
 
     frame = cv.flip(frame, 1)
 
+    #making rectangle to mark the region of interest which will be detected by board
     cv.rectangle(frame, (500, 50), (800, 350), (255, 0, 0), 2)
+
     roi = frame[50:350, 500:800, :] # ROI : Region of Interest
     roi, contours, roi_range = get_ict(roi)
 
     draw_started = False
     draw_stopped = False
 
+    # confirming if any contour exists or not
     if len(contours)>0:
         draw_started = True
+
+        # Finding the largest contour based on area
         max_contour = max(contours, key=cv.contourArea)
 
+        # marks the center of moment of the contour detected
         M = cv.moments(max_contour)
 
+        # using the center of moment to find put the centroid of the contour
         try:
             center = (int(M['m10'] / M['m00']), int(M['m01'] / M['m00']))
         except:
             continue
+
         # center obtained is appended to the deque
         centerpoints.appendleft(center)
     else:
         draw_stopped= True
 
+    # marking the detected points on the ROI image and then plotting on the board
     for i in range(1, len(centerpoints)):
+
+    	# distance between the two nearest detected points
         if math.sqrt((centerpoints[i-1][0] - centerpoints[i][0])**2 + (centerpoints[i-1][1] - centerpoints[i][1])**2) < 50:
+
             cv.line(roi, centerpoints[i-1], centerpoints[i], (200, 200, 200), 5, cv.LINE_AA)
             cv.line(board, (centerpoints[i-1][0]+15, centerpoints[i-1][1]+15), (centerpoints[i][0]+15, centerpoints[i][1]+15),
                     255, 7, cv.LINE_AA)
 
+    # resizing the board to fit image in the classifier
     img = cv.resize(board, (28, 28))
 
     if np.max(board) != 0 and draw_started== True and draw_stopped== True:
+    	# works like erosion and dilusion. Enchances white and dark according to values passed.
         img = cv.morphologyEx(img, cv.MORPH_OPEN, (5, 5))
         board = cv.morphologyEx(board, cv.MORPH_OPEN, (5, 5))
 
         draw_started = False
         draw_stopped = False
 
+    # If something green is detected on the board:
     if np.max(board) != 0:
         img = np.array(img)
 
         hog_features = hog(img, orientations=9, pixels_per_cell=(14, 14), cells_per_block=(1, 1), block_norm='L2')
         hog_features = preproc.transform(np.array([hog_features], 'float64'))
 
+        # predicting the final result
         result = clf.predict(hog_features)
     
     if int(result[0])<0:
@@ -85,20 +116,26 @@ while cap.isOpened():
         cv.putText(frame, "Predicted number: " + str(int(result)), (5, 420),
                    cv.FONT_HERSHEY_DUPLEX, 1, (255, 255, 0), 2)
     
-
+    # displays the images as windows   
     cv.imshow('frame', frame)
     #cv.imshow('roi', roi)
     #cv.imshow('roi_image', roi_range)
     cv.imshow('board', board)
     cv.imshow('img', img)
+
+    # waitkey waits for the input before processing next frame
     k = cv.waitKey(1) & 0xFF
     if k == ord('q'):
+    	# Quit when 'Q' is pressed
         break
+
+    #clear the board and result when 'c' is pressed
     elif k == ord('c'):
         board.fill(0)
         centerpoints.clear()
         result = [-2, -1]
-    # clearing the board
+    
+# Destroying the windows after end of program    
 cap.release()
 cv.destroyAllWindows()
 
--- model_train.py
+++ model_train.py
@@ -1,44 +1,64 @@
-import numpy as np
-from sklearn.svm import SVC
-from tensorflow import keras
+import numpy as np # for matrix calculations
+from sklearn.svm import SVC # SVM classifier
+from tensorflow import keras # for importing dataset
 from sklearn.externals import joblib  # for saving our model
 from sklearn import preprocessing  # for standardizing the dataset
 # Extract Histogram of Oriented Gradients (HOG) for a given image.
 from skimage.feature import hog
 
+# loading data from MNIST dataset in Keras in 2 parts, training and testing data
 (traindata, trainlabel), (testdata, testlabel) = keras.datasets.mnist.load_data()
+
+# checking the shapes of data
 print(traindata.shape, trainlabel.shape, testdata.shape, testlabel.shape)
 
+# Initializing the hog feature list
 hog_feature_list = []
 hog_test = []
 
+
+# converting each training example into respestive hog values
 for data in traindata:
     ft = hog(data, orientations=9, pixels_per_cell=(
         14, 14), cells_per_block=(1, 1), block_norm='L2')
     hog_feature_list.append(ft)
 
+
+# converting each test example into respestive hog values
 for data in testdata:
     ft = hog(data, orientations=9, pixels_per_cell=(
         14, 14), cells_per_block=(1, 1), block_norm='L2')
     hog_test.append(ft)
 
+# converting hog features into array of float values
 hog_features = np.array(hog_feature_list, 'float64')
 hog_tests = np.array(hog_test, 'float64')
 
+# confirming the shapes
 print(hog_features, hog_features.shape)
 
+# Preprocessing the data using StandardScaler 
+# It normalizes the data, thus making it easier to carry out the calculations.
 preproc = preprocessing.StandardScaler().fit(hog_features)
+
+# Transforming both train and test data into preprocessed data.
 hog_features = preproc.transform(hog_features)
 hog_tests = preproc.transform(hog_tests)
 print(hog_features, hog_features.shape)
 
+# Making the classifier using SVM
 clf = SVC(C=5, gamma=0.05)
+
+# Fitting the training features into the classifier and checking the score.
 clf.fit(hog_features, trainlabel)
 
+# Score of the training examples
 print(clf.score(hog_features, trainlabel))
 
+# Saving the model (classifier along with preprocessed data) using the dump()
 joblib.dump((clf, preproc), "mnist_clf_svm.pkl", compress=3)
 #  Higher value means more compression,
 # but also slower read and write times. Using a value of 3 is often a good compromise
 
+# Checking the result on the test data, thus checking the performance of the model on unseen data
 print(clf.score(hog_tests, testlabel))
